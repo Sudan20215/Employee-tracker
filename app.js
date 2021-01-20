d454705eb0909abc62ec54882e509f9892d70772
sudan bhusal <sudanbhusal20215@gmail.com>
2021-01-20 08:15:32 -0800

    prompt from the user

--- app.js
+++ app.js
@@ -16,3 +16,59 @@ con.connect(function (err) {
   console.log("Connected as id " + con.threadId + "\n");
   HomeView();
 });
+function HomeView() {
+    inquirer
+      .prompt([
+        {
+          message: "Whate would you like to do? (Use arrow keys)",
+          type: "list",
+          choices: [
+            "View all Employee",
+            "View All Employee By Department",
+            "View All Employees By Role",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "View the total utilized budget of a department",
+            "Exit\n",
+          ],
+          name: "task",
+        },
+      ])
+      .then(function (answer) {
+        var task = answer.task;
+        switch (task) {
+          case "View all Employee":
+            DetailTableView();
+            break;
+          case "View All Employee By Department":
+            EmployeeByDepartment();
+            break;
+          case "View All Employees By Role":
+            viewEmployeeByRole();
+            break;
+          case "Add Employee":
+            addEmployee();
+            break;
+          case "Remove Employee":
+            deleteEmployee();
+            break;
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+          case "Update Employee Manager":
+            updateEmployeeManager() 
+            break;
+          case "Add Roles":
+            addRole();
+            break;
+          case "View the total utilized budget of a department":
+            viewTotalUtilizedBudget()
+            break;
+          case "Exit \n":
+            exit();
+            break;
+        }
+      });
+  }  
